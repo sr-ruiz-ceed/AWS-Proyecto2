4a1f70528ce3b8bfaa984bc21606117f415d1f26
sr-ruiz-ceed <jr.ruizrodriguez@edu.gva.es>
2026-01-07 18:47:59 +0100

    Update index.php

--- index.php
+++ index.php
@@ -64,6 +64,7 @@ if ($_SERVER['REQUEST_METHOD'] === 'POST' && isset($_POST['title'])) {
     <meta charset="UTF-8">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
     <title>Gestor de Activos - AWS</title>
+    <link rel="stylesheet" href="styles.css">
 </head>
 <body>
 
